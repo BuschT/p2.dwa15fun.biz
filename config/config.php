d7193e8333c8397d966fdd517ef60814fa6cf457
Taylor Busch <Taylor.R.Busch.-ND@espn.com>
2013-11-03 18:14:06 -0500

    update system email

--- config/config.php
+++ config/config.php
@@ -28,7 +28,7 @@ A email designated to receive messages from the server. Examples:
 
  	Some might want this to be the same as the APP_EMAIL, others might want to create a designated gmail address for it
 */
-	define('SYSTEM_EMAIL', 'webmaster@myapp.com');
+	define('SYSTEM_EMAIL', 'Taylorbusch@gmail.com');
 
 # Default DB name for this app
 	define('DB_NAME', 'dwafunbi_p2_dwa15fun_biz');
