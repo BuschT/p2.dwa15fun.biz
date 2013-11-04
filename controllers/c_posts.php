e96068ec69fabeeee2205be6da13b955dc3dc251
Taylor Busch <Taylor.R.Busch.-ND@espn.com>
2013-11-03 21:32:01 -0500

    basic page structure

--- controllers/c_posts.php
+++ controllers/c_posts.php
@@ -1,3 +1,4 @@
+<?php
 class posts_controller extends base_controller {
 
     public function __construct() {
--- views/_v_template.php
+++ views/_v_template.php
@@ -2,8 +2,14 @@
 <html>
 <head>
 	<title><?php if(isset($title)) echo $title; ?></title>
-
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+
+	<!-- Bootstrap -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="/css/jumbotron-narrow.css" rel="stylesheet">
 
 	<!-- Controller Specific JS/CSS -->
 	<?php if(isset($client_files_head)) echo $client_files_head; ?>
@@ -12,30 +18,47 @@
 
 <body>
 
-	<div id='menu'>
+	<div class="container">
+	  <div class="header">
+		<ul class="nav nav-pills pull-right">
+		  <!-- <li class="active"><a href="#">Home</a></li> -->
+		  <li><a href="/">Home</a></li>
+		  <!-- Menu for users who are logged in -->
+			<?php if($user): ?>
+				<li><a href='/users/logout'>Logout</a></li>
+				<li><a href='/users/profile'>Profile</a></li>
+			<!-- Menu options for users who are not logged in -->
+			<?php else: ?>
+				<li><a href='/users/signup'>Sign up</a></li>
+				<li><a href='/users/login'>Log in</a></li>
+			<?php endif; ?>
+		</ul>
+		<h3 class="text-muted">BlogTastic!</h3>
+	  </div>
+	  <div class="content_options">
+	  	<ul class="nav nav-pills">
+	  		<li><a href="/users">All Users</a></li>
+	  		<li><a href="/posts/users">Users You Follow</a></li>
+	  	</ul>
+	  </div>
+	  <div class="jumbotron">
+		 <!-- Content! -->
+		 <?php if(isset($content)) echo $content; ?>
+	  </div>
 
-		<a href='/'>Home</a>
+	  <div class="footer">
+		<p>&copy; BlogTastic! 2013</p>
+	  </div>
 
-		<!-- Menu for users who are logged in -->
-		<?php if($user): ?>
-
-			<a href='/users/logout'>Logout</a>
-			<a href='/users/profile'>Profile</a>
-
-		<!-- Menu options for users who are not logged in -->
-		<?php else: ?>
-
-			<a href='/users/signup'>Sign up</a>
-			<a href='/users/login'>Log in</a>
-
-		<?php endif; ?>
-
-	</div>
+    </div> <!-- /container -->
 
     <br>
 
-	<?php if(isset($content)) echo $content; ?>
-
 	<?php if(isset($client_files_body)) echo $client_files_body; ?>
+
+	<!-- placed at end of file so code page loads faster -->
+	<script src="https://code.jquery.com/jquery.js"></script>
+	<script src="/js/bootstrap.min.js"></script>
+
 </body>
 </html>
