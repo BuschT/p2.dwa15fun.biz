974ae9a514a6bb8867be409f0029b5e72c9fa032
Taylor Busch <Taylor.R.Busch.-ND@espn.com>
2013-11-05 22:31:43 -0500

    add welcome message

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,4 +1,5 @@
 <p>
 	Welcome To BlogTastic!<br /><br />
-	My +1 Features are the ability to delete and edit posts!
+	My +1 Features are the ability to delete and edit posts!<br /><br />
+	This page probably looks best in Chrome, which is what I mainly designed it for.
 </p>
